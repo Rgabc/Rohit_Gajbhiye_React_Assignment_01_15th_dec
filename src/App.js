f60b41dcc85e9a879ac52ba93fd90776cc32f4e4
Rgabc <rohitgajbhiye4@gmail.com>
2023-12-15 15:57:56 +0530

    react1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import NavBar from "./component/NavBar";
+import Portfolio from "./component/Portfolio";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <NavBar/>
+      <Portfolio/>
+      <About/>
+      <Contact/>
+      <Footer/>
+    
     </div>
   );
 }
